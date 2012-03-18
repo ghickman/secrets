82272191fe216fa510ca57a6fcf4ea0b34a5a81e
George Hickman <george@ghickman.co.uk>
2012-03-18 15:11:00 +0000

    Add a blank home page

--- secrets/urls.py
+++ secrets/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'secrets.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='core/home.html'), name='home'),
     # url(r'^secrets/', include('secrets.foo.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
