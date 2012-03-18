0ddf8906bdccec9dee8ac7a1addf801c470b7159
George Hickman <george@ghickman.co.uk>
2012-03-18 22:50:08 +0000

    Return an app's secrets in list and detail views.

--- secrets/core/api.py
+++ secrets/core/api.py
@@ -13,6 +13,15 @@ class AppResource(ModelResource):
         authorization = DjangoAuthorization()
         queryset = App.objects.all().select_related()
 
+    def alter_detail_data_to_serialize(self, request, data):
+        data.data.update({'secrets': self.secrets_related_to(data.obj)})
+        return super(AppResource, self).alter_detail_data_to_serialize(request, data)
+
+    def alter_list_data_to_serialize(self, request, data):
+        for i, obj in enumerate(data['objects']):
+            data['objects'][i].data.update({'secrets': self.secrets_related_to(obj.obj)})
+        return super(AppResource, self).alter_list_data_to_serialize(request, data)
+
     def override_urls(self):
         """
         http://django-tastypie.readthedocs.org/en/latest/cookbook.html#using-non-pk-data-for-your-urls
@@ -22,6 +31,9 @@ class AppResource(ModelResource):
                 self.wrap_view('dispatch_detail'), name='api_dispatch_detail'),
         ]
 
+    def secrets_related_to(self, app_obj):
+        return [{'id': secret.pk, 'name': secret.name, 'secret': secret.secret}
+               for secret in Secret.objects.filter(app=app_obj)]
 
 class SecretResource(ModelResource):
     app = ForeignKey(AppResource, 'app')
