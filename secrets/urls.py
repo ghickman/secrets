88f2d259f3c90d417f5d07684fbdba73f0c83707
George Hickman <george@ghickman.co.uk>
2012-03-21 21:48:04 +0000

    Deal with the favicon

--- secrets/urls.py
+++ secrets/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
+from django.views.generic import RedirectView, TemplateView
 
 from core.api import v1_api
 
@@ -12,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^api/', include(v1_api.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico'), name='favicon'),
 )
