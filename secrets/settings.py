bec1788cf93daff1305582f3aab683b8529c42c6
George Hickman <george@ghickman.co.uk>
2012-06-03 12:37:30 +0100

    Tidy up ready for 1.4 update

--- secrets/settings.py
+++ secrets/settings.py
@@ -1,62 +1,55 @@
 # Django settings for secrets project.
-from os import environ
-from os.path import abspath, dirname, join
+import os
 
-DIRNAME = abspath(dirname(__file__))
+import dj_database_url
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    ('George Hickman', 'george@ghickman.co.uk'),
-)
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
+DEBUG = bool(os.environ.get('DEBUG', False))
+
+ADMINS = (('George Hickman', 'george@ghickman.co.uk'),)
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost/secrets')
 }
 
 TIME_ZONE = 'Europe/London'
+USE_TZ = True
+
 LANGUAGE_CODE = 'en-gb'
-
-SITE_ID = 1
-
-USE_I18N = True
-USE_L10N = True
+USE_I18N = True  # Internationalization
+USE_L10N = True  # Locale
 
 # S3 Backend
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-# Static Media
+# Static
 MEDIA_ROOT = 'client_media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = 'static_media'
 STATIC_URL = 'http://{0}.s3.amazonaws.com/'.format(AWS_STORAGE_BUCKET_NAME)
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-STATICFILES_DIRS = (join(DIRNAME, 'static'),)
+STATICFILES_DIRS = (os.path.join(DIRNAME, 'static'),)
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
-SECRET_KEY = 'pl#%lb5=ws784n%ioe@+1f*s_1**e#g4f225*pr0&hjll6kw%q'
-
+TEMPLATE_DIRS = (join(DIRNAME, 'templates'))
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
+
+ROOT_URLCONF = 'secrets.urls'
+SECRET_KEY = 'pl#%lb5=ws784n%ioe@+1f*s_1**e#g4f225*pr0&hjll6kw%q'
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -66,11 +59,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'secrets.urls'
-
-TEMPLATE_DIRS = (join(DIRNAME, 'templates'))
-
 INSTALLED_APPS = (
+    'core',
+
+    'django_simple_aes_field',
+    'storages',
+    'tastypie',
+
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -78,21 +73,20 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-
-    'django_simple_aes_field',
-    'gunicorn',
-    'storages',
-    'tastypie',
-
-    'core',
 )
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
@@ -105,7 +99,3 @@ LOGGING = {
     }
 }
 
-try:
-    from local_settings import *
-except ImportError as e:
-    pass
