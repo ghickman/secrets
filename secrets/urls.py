69be05b119c567cc017eb87a26040f1d302207c7
George Hickman <george@ghickman.co.uk>
2012-06-22 19:48:45 +0100

    Add static bits

--- secrets/urls.py
+++ secrets/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import RedirectView, TemplateView
 
 from core.api import v1_api
@@ -15,3 +16,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico'), name='favicon'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
