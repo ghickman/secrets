d848b209eb06f517fa3bb8369953fb8654223399
George Hickman <george@ghickman.co.uk>
2012-06-22 19:46:49 +0100

    Clean up imports

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
-from secrets.core.models import App, Secret
+from .models import App, Secret
 
 
 class SecretInline(admin.TabularInline):
